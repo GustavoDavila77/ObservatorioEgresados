2bbb48997bb615de5021593a855f0f1539043b2b
Nicolas Orozco <nicolas.orozco@utp.edu.co>
2020-05-04 15:03:13 -0500

    email sender ready

--- src/routes/forgotpassword.js
+++ src/routes/forgotpassword.js
@@ -1,9 +1,8 @@
 /// Aqui estan las rutas para autenticarse
 const express = require('express');
 const router = express.Router(); //para creación de rutas
-const nodemailer = require('nodemailer');
 const SuperUser = require('../models/superuser');
-const { content } = require('googleapis/build/src/apis/content');
+const nodemailer = require('nodemailer');
 
 
 function generar()
@@ -22,15 +21,36 @@ router.post('/forgotpassword/getdata', async (req, res) =>  {
     const {email, dni } = req.body;
     const emailUser = await SuperUser.findOne({email: email});
     const DNIUser = await SuperUser.findOne({dni: dni});
-    contentHTML = '<h2>Nueva clave</h2>'+
-    '<ul><li>Clave:'+generar()+'</li></ul>';
-    //nodemailer.createTransport({})
+    contentHTML = '<h2>Nueva clave</h2><ul><li>Clave:  '+generar()+'</li></ul>';
+    //Creamos el objeto de transporte
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        host: 'smtp.gmail.com',
+        port: 587,
+        auth: {
+                user: 'projectslabegresados@gmail.com',
+                pass: 'Niko_orozco'
+            }
+    });
+
+    var mailOptions = {
+        from: 'projectslabegresados@gmail.com',
+        to: {email},
+        subject: 'Recuperar contraseña',
+        html: contentHTML
+    };
+
     if(emailUser /*&& DNIUser*/) {
-        console.log(contentHTML);
+        await transporter.sendMail(mailOptions, function(error, info){
+            if (error) {
+                console.log(error);
+            } else {
+                console.log('Email enviado: ' + info.response);
+            }
+        });
         res.redirect('/');
     }else{
-        req.flash('error_msg', 'El correo es incorrecto o');
-        req.flash('error_msg', ' el DNI es incorrecto intente nuevamente');
+        req.flash('error_msg', 'El correo es incorrecto o el DNI es incorrecto intente nuevamente');
         res.redirect('/forgotpassword');
     }
     
